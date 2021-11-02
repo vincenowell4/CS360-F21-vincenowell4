c570af394d98fe6951ab19d4075be2c85fdd5b07
vincenowell4 <81654229+vincenowell4@users.noreply.github.com>
2021-11-02 13:20:31 -0700

    Initial C code - no changes made yet...

---
+++ Lab4/MP3HeaderInfo/Lab4.c
@@ -0,0 +1,54 @@
+/* CS360 Lab 4: C */
+
+#include <stdio.h>
+
+int main( int argc, char ** argv )
+{
+	// Open the file given on the command line
+	if( argc != 2 )
+	{
+		printf( "Usage: %s filename.mp3\n", argv[0] );
+		return(EXIT_FAILURE);
+	}
+	
+	FILE * fp = fopen(argv[1], "rb");
+	if( fp == NULL )
+	{
+		printf( "Can't open file %s\n", argv[1] );
+		return(EXIT_FAILURE);
+	}
+	
+	// How many bytes are there in the file?  If you know the OS you're
+	// on you can use a system API call to find out.  Here we use ANSI standard
+	// function calls.
+	long size = 0;
+	fseek( fp, 0, SEEK_END );		// go to 0 bytes from the end
+	size = ftell(fp);				// how far from the beginning?
+	rewind(fp);						// go back to the beginning
+	
+	if( size < 1 || size > 10485760 )
+	{
+		printf("File size is not within the allowed range\n"); 
+		goto END;
+	}
+	
+	printf( "File size: %d MB\n", size/1048576 );
+	// Allocate memory on the heap for a copy of the file
+	unsigned char * data = (unsigned char *)malloc(size);
+	// Read it into our block of memory
+	size_t bytesRead = fread( data, sizeof(unsigned char), size, fp );
+	if( bytesRead != size )
+	{
+		printf( "Error reading file. Unexpected number of bytes read: %d\n",bytesRead );
+		goto END;
+	}
+	
+	// We now have a pointer to the first byte of data in a copy of the file, have fun
+	// unsigned char * data    <--- this is the pointer
+	
+	
+	// Clean up
+END:
+	fclose(fp);				// close and free the file
+	exit(EXIT_SUCCESS);		// or return 0;
+}
